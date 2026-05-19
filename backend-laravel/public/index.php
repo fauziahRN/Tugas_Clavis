c47e75cd44854dd662840dfd7ac8b6b4951c1b16
Fauziah Fauziah <fzhnadin@gmail.com>
2026-05-19 12:40:08 +0700

    fix duplicate cors headers

--- backend-laravel/public/index.php
+++ backend-laravel/public/index.php
@@ -11,15 +11,6 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 }
 
 // Register the Composer autoloader...
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
-
 require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
